06a9fd36b9d31371b478dbd8b06587b3dacb53b1
mariones158 <mariones158@gmail.com>
2020-02-17 19:12:51 -0300

    migraciones

--- mecenazgoDH/database/migrations/2014_02_10_015543_create_users_table.php
+++ mecenazgoDH/database/migrations/2014_02_10_015543_create_users_table.php
@@ -15,12 +15,18 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_type_id');
             $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('indenfication_number');
+            $table->string('avatar')->nullable();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->decimal('amount', 5, 3);
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
---
+++ mecenazgoDH/database/migrations/2020_02_17_204200_create_projects_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('title', 100);
+            $table->string('image');
+            $table->string('colour');
+            $table->text('description');
+            $table->decimal('amount', 5, 3);
+            $table->boolean('status');
+            $table->text('description_detail')->nullable();
+            $table->text('description_general')->nullable();
+            $table->string('description_title')->nullable();
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
